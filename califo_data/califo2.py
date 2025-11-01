ff6cf6af5f65a62c6938fb596954649cfa89bf17
mohmet-bey <orhanmehmet2013@gmail.com>
2025-11-01 16:54:45 +0300

    change

--- califo_data/califo2.py
+++ califo_data/califo2.py
@@ -5,6 +5,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import r2_score
 
+
 ## Page layout
 ## Page expands to full width
 st.set_page_config(page_title='The Machine Learning App for Streamlit', layout='wide', page_icon="random")
